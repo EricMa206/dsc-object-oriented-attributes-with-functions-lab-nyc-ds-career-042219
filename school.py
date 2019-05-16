bff85985f8a576c814b5cbbb4866e47dc6a94bd9
Eric <eric.ma206@gmail.com>
2019-05-16 10:17:05 -0400

    -

--- school.py
+++ school.py
@@ -1,1 +1,14 @@
 class School:
+
+    def __init__(self, school_name, roster={9: [], 10: [], 11: [], 12: []}):
+        self.school_name = school_name
+        self.roster = roster
+
+    def add_student(self, full_name, grade):
+        self.roster[grade].append(full_name)
+
+    def grade(self, grade):
+        return self.roster[grade]
+
+    def sort_roster(self):
+        return sorted(self.roster.values())
